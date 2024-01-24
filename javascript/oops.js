af9f815297c4afb5fc020cffaf64145468532236
Raghav Nayak <ragsnayak@gmail.com>
2024-01-24 22:25:46 +0530

    added oops examples

--- javascript/oops.js
+++ javascript/oops.js
@@ -4,8 +4,31 @@ const user = {
     signedIn: true,
 
     getUserDetails: function () {
-        console.log("Got user details from DB : ", this.userName);
+        console.log("Got user details from DB : ", this);
     }
 }
 
-user.getUserDetails()
+user.getUserDetails();
+
+// const promiseOne = new Promise();
+// const date = new Date();
+
+
+function User(userName, loginCount, signedIn) {
+    this.userName = userName;
+    this.loginCount = loginCount;
+    this.signedIn = signedIn;
+
+    this.greeting = function () {
+        console.log("Greeting from User ", this.userName);
+    }
+}
+
+const userOne = new User("raghav", 12, true);
+const userTwo = new User("raghavan", 11, false);
+console.log("userOne: ", userOne);
+console.log("userTwo: ", userTwo);
+userTwo.greeting();
+
+
+console.log("constructor: ", userTwo.constructor);
